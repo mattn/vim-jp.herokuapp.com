598eb43a1920d6a260b5156093f946d574af1816
mattn <mattn.jp@gmail.com>
2013-07-29 10:51:23 +0900

    update

--- web.go
+++ web.go
@@ -19,7 +19,7 @@ import (
 	"time"
 )
 
-const uri = "http://ftp.vim.org/vim/unstable/patches/7.4a/"
+const uri = "http://ftp.vim.org/vim/unstable/patches/7.4b/"
 
 type Status struct {
 	Events []Event `json:"events"`
